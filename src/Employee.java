d307f214e0925f0a9bbdb3219328301b237e7ec6
ludmila.nistor <nistor.ludmila@gmail.com>
2023-10-11 17:21:50 +0300

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,62 @@
+public class Employee {
+
+//    String name;
+//    int age;
+//    String department;
+
+    private String name;
+    private int age;
+    private String department;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        
+    }
+
+
+    public int getAge(){
+        return age;
+    }
+
+  public String getDepartment(){
+        return department;
+  }
+
+
+
+    /*
+    public class Employee {
+   private  String name;
+   private int age;
+   private String department;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age < 18) {
+            System.out.println("Nu angajam persoane minore");
+        } else {
+            this.age = age;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+     */
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Membrii Employee");    }
+
+    Employee employee = new Employee();
+     employee.nam
+
+
+
+    /*
+    public class Main {
+    public static void main(String[] args) {
+
+        Employee ani=new Employee();
+        ani.setAge(17);
+        System.out.println(ani.getAge());
+    }
+}
+     */
+}
