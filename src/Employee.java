abfadcf444c08a7dfc527fc83a3450ae1aff0ddf
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-13 07:08:03 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,62 +1,34 @@
 public class Employee {
-
-//    String name;
-//    int age;
-//    String department;
-
     private String name;
-    private int age;
-    private String department;
-
-    public String getName(){
-        return name;
-    }
-    public void setName(){
-        
-    }
-
-
-    public int getAge(){
-        return age;
-    }
-
-  public String getDepartment(){
-        return department;
-  }
-
-
-
-    /*
-    public class Employee {
-   private  String name;
-   private int age;
-   private String department;
-
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        if (age < 18) {
-            System.out.println("Nu angajam persoane minore");
-        } else {
-            this.age = age;
-        }
-    }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
-        this.name = name;
-    }
+        this.name = name;    }
 
+    private int age;
+   public Employee (int age) {}
+//        if (age > 0) {
+//            this.age = age;
+//        } else {
+//            System.out.println("Stagiu de lucru mai putin de un an nu sunt");
+//        }
+//    }
+  public void setAge(int age) {
+        if (age > 0) {
+        this.age = age;
+    } else {
+        System.out.println("Stagiu de lucru mai putin de un an nu sunt");
+    }
+}
+    public int getAge() {
+        return age;}
+
+    private String department;
     public String getDepartment() {
         return department;
     }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-     */
+    public void setDepartment(String department){
+        this.department = department; }
 
 }
