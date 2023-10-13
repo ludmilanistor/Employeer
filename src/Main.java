733fffdb3071c7c7fc2fb693995f2be128145486
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-13 10:05:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Membrii Employee");
 
-            Employee employee = new Employee(17);
+            Employee employee = new Employee(0);
 
         
         System.out.println("Ani lucrati " + employee.getAge());
