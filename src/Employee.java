532222db181073842bba2af1e0fa2c1e20a51148
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-13 15:58:26 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,39 +1,35 @@
 public class Employee {
-
-private String name;
-    public String getName() {
-        return name;
-    }
+    private String name;
+    //public String getName() {
+      //  return name;}
     public void setName(String name) {
         this.name = name;    }
 
     public  String getName(){
         return name;
     }
-    
-  private int age;
-    
-   public Employee (int age) {}
-        if (age > 0) {
-            this.age = age;
+
+    private int age;
+
+    public Employee (int age) {
+          if (age > 0) {
+          this.age = age;
        } else {
-           System.out.println("Stagiu de lucru mai putin de un an nu sunt");
-       }
-   }
-  public void setAge(int age) {
-        if (age > 0) {
-        this.age = age;
-    } 
-      
-    public int getAge() {
+            System.out.println("Stagiu de lucru mai putin de un an nu sunt");
+        }
+    }
+public void setAge(int age) {
+        this.age = age;}
+
+     public int getAge() {
         return age;}
 
 
-private String department;
-    public String getDepartment() {
-        return department;
-    }
+    private String department;
     public void setDepartment(String department){
         this.department = department;}
 
+    public String getDepartment() {
+        return department;
+    }
 }
