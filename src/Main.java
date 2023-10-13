00842c8831125283519f39ebcffd90d0764dfb39
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-13 15:59:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Membrii Employee");
+                System.out.println("");
 
             Employee employee = new Employee(0);
 
